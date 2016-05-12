49f46bd0f33f15df00d68a1aa6364dac377c2a76
a4a881d4 <a4a881d4@163.com>
2016-05-13 07:26:41 +0800

    start ad9361 bug

--- src/python/spectrum.py
+++ src/python/spectrum.py
@@ -1,6 +1,9 @@
 import web
 import os
 import axi2s_u
+import AD9361_c
+import adscripts
+import axi2s_c
 
 urls = ('/','index','/data','data')
 class index:
@@ -12,7 +15,17 @@ class data:
 		r = ocm.idata()
 		return r
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
+	path = os.path.split(os.path.realpath(__file__))[0]
+	fn = path+"/../../AD9361/ad9361_config.reg"
+	f = open(fn)
+	ad = AD9361_c.AD9361_c()
+	ad.writereg('AD9361_EN',1)
+	for x in f.readlines():
+		adscripts.parse(x,ad.order)
+	uut = axi2s_c.axi2s_c()
+	uut.init()
+
 	app = web.application(urls, globals())
 	app.run()
 
