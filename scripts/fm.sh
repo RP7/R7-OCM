5168b246d34bca0d12ab94974389f8fb7db5e771
a4a881d4 <a4a881d4@163.com>
2016-06-12 23:23:10 +0800

    FM scripts

--- scripts/fm.sh
+++ scripts/fm.sh
@@ -6,7 +6,11 @@ echo ""
 echo "set rx fir"
 curl -F chead=@'AD9361/30K_75K.h' http://${hostip}:8080/fir
 echo ""
-echo "init"
-curl http://${hostip}:8080/init?'rx&freq=103.9e6'
+echo "set freq set agc"
+curl http://${hostip}:8080/init?'rx&freq=97.4e6&gain=35'
+echo ""
+sleep 1
+echo "start FM"
+curl http://${hostip}:8080/FM?start
 echo ""
 echo "finished"
