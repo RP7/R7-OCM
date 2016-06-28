39ee5afeac17972ba33341d36cd312ce19bcd69a
a4a881d4 <a4a8881d4@163.com>
2016-06-29 02:27:16 +0800

    wireshark ok

--- src/host/python/gsmlib/gsmtap.py
+++ src/host/python/gsmlib/gsmtap.py
@@ -2,42 +2,43 @@ from ctypes import *
 from socket import socket,AF_INET,SOCK_DGRAM
 
 class gsmtap_hdr(Structure):
-	__pack__ = 1
-	__fields__ = [   ("version", c_int8)
+	_pack_ = 1
+	_fields_ = [   ("version", c_int8)
 		, ("hdr_len", c_int8)
 		, ("type", c_int8)
 		, ("timeslot", c_int8)
 		, ("arfcn", c_int16)
 		, ("signal_dbm", c_int8)
+		, ("snr_db", c_int8)
 		, ("frame_number", c_int32)
 		, ("sub_type", c_int8)
+		, ("antenna_nr", c_int8)
 		, ("sub_slot", c_int8)
 		, ("res", c_int8)
 	]
 
 class gsmtap_data(Structure):
-	__pack__ = 1
-	__fields__ = [   ("hdr", gsmtap_hdr)
+	_pack_ = 1
+	_fields_ = [   ("header", gsmtap_hdr)
 		, ("data",c_int8*23)
 	]
 
 class gsmtap:
 	def __init__(self):
-		self.package = gsmtap_data()
-		
-		self.package.hdr.version = 1
-		self.package.hdr.hdr_len = 23
-		self.package.hdr.type = 1
-		self.package.hdr.signal_dbm = -40
-		self.package.hdr.sub_type = 1
+		self.block = gsmtap_data()
+
+		self.block.header.version = 1
+		self.block.header.hdr_len = 4
+		self.block.header.type = 1
+		self.block.header.signal_dbm = -40
+		self.block.header.sub_type = 1
 		self.sock = socket(AF_INET, SOCK_DGRAM)
 
 	def send(self,data):
-		self.package.data[:]=data[:]
-		self.sock.sendto(('127.0.0.1',4729),self.struct2stream(self.package))
+		self.block.data[:]=data[:]
+		self.sock.sendto(self.struct2stream(self.block),('127.0.0.1',4729))
 
 	def struct2stream(self,s):
 		length  = sizeof(s)
 		p       = cast(pointer(s), POINTER(c_char * length))
 		return p.contents.raw
-
